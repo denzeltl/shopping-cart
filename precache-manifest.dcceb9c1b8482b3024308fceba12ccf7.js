1d1bc0f7c81df158bf021a58e5cd144a8e760c64
Denzel <denzeltlee@gmail.com>
2020-05-11 23:06:52 +0800

    Updates

---
+++ precache-manifest.dcceb9c1b8482b3024308fceba12ccf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0d53aa615d94b8fbe01c92469a6222c",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "f8dbf473970c013660f8",
+    "url": "/shopping-cart/static/css/main.67ef68d9.chunk.css"
+  },
+  {
+    "revision": "e3383820e5b9ddef054b",
+    "url": "/shopping-cart/static/js/2.f5328773.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopping-cart/static/js/2.f5328773.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8dbf473970c013660f8",
+    "url": "/shopping-cart/static/js/main.66a0c02d.chunk.js"
+  },
+  {
+    "revision": "f78ee7b71997a9b5e1df",
+    "url": "/shopping-cart/static/js/runtime-main.2044215a.js"
+  }
+]);
